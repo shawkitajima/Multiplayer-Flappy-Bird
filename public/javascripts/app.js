abc4377a82ee908e65d8a66ab628af3396a0320e
Shaw Kitajima <shawkitajima@gmail.com>
2020-01-07 15:49:35 -0800

    implements moving pipes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,20 +1,21 @@
 const socket = io();
 const canvas = document.getElementById('game-board');
 const ctx = canvas.getContext('2d')
-let raf;
 
 // y direction velocity increaser
 const grav = 0.05;
+const gap = 125;
+let score = 0;
 
 // listens for space and makes bird jump
 document.addEventListener('keydown', function(evt) {
-    if (evt.which === 32) {
+    if (evt.which === 65) {
         bird.vy -= 2;
     }
 })
     
 const bird = {
-    x: 100,
+    x: 250,
     y: 100,
     vx: 0,
     vy: 1,
@@ -29,21 +30,54 @@ const bird = {
     }
 };
 
+const bottomPipe = {
+    x: 900,
+    y: Math.floor(Math.random() * (900 - 400 + 1) + 300),
+    vx: 4,
+    draw: function() {
+        ctx.fillStyle = 'green';
+        ctx.fillRect(this.x, this.y, 100, 1000);
+    }
+}
+
+const topPipe = {
+    x: 900,
+    y: bottomPipe.y - gap,
+    vx: 4,
+    draw: function() {
+        ctx.fillStyle = 'green';
+        ctx.fillRect(this.x, this.y, 100, -this.y);
+    }
+}
+
 function init() {
     bird.draw();
-    raf = window.requestAnimationFrame(draw);
+    window.requestAnimationFrame(draw);
 }
 
 function draw() {
     ctx.clearRect(0,0, canvas.width, canvas.height);
     bird.draw();
+    bottomPipe.draw();
+    topPipe.draw();
+    bottomPipe.x -= bottomPipe.vx;
+    topPipe.x -= topPipe.vx;
     bird.vy += grav;
     bird.y += bird.vy;
+    // Let's generate a new set of pipes and add to the score
+    if (bottomPipe.x <= -100) {
+        bottomPipe.x = 900;
+        topPipe.x = 900;
+        bottomPipe.y = Math.floor(Math.random() * (900 - 400 + 1) + 300);
+        topPipe.y = bottomPipe.y - gap;
+        score++;
+        document.getElementById('score').innerHTML = `Score: ${score}`
+    }
+    // We can rotate the bird by checking if vy is positive or negative
     if (bird.y > canvas.height || bird.y < 0) {
-        console.log('fire!');
         location.reload();
     }
-    raf = window.requestAnimationFrame(draw);
+    window.requestAnimationFrame(draw);
 }
 
 
