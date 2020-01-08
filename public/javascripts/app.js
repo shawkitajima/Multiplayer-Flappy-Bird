45c5a3121170a0a076652ec00418001488b8446d
Shaw Kitajima <shawkitajima@gmail.com>
2020-01-07 22:07:50 -0800

    add collision detection

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,7 +5,9 @@ const ctx = canvas.getContext('2d')
 // y direction velocity increaser
 const grav = 0.05;
 const gap = 125;
+const pipeStart = 1500;
 let score = 0;
+
 
 // listens for space and makes bird jump
 document.addEventListener('keydown', function(evt) {
@@ -32,9 +34,10 @@ const bird = {
 };
 
 const bottomPipe = {
-    x: 1200,
+    x: pipeStart,
     y: Math.floor(Math.random() * (900 - 400 + 1) + 300),
     vx: 4,
+    id: 'bot',
     draw: function() {
         ctx.fillStyle = 'green';
         ctx.fillRect(this.x, this.y, 100, 1000);
@@ -42,9 +45,10 @@ const bottomPipe = {
 }
 
 const topPipe = {
-    x: 1200,
+    x: pipeStart,
     y: bottomPipe.y - gap,
     vx: 4,
+    id: 'top',
     draw: function() {
         ctx.fillStyle = 'green';
         ctx.fillRect(this.x, this.y, 100, -this.y);
@@ -67,21 +71,52 @@ function draw() {
     bird.y += bird.vy;
     // Let's generate a new set of pipes and add to the score
     if (bottomPipe.x <= -100) {
-        bottomPipe.x = 1200;
-        topPipe.x = 1200;
+        bottomPipe.x = pipeStart;
+        topPipe.x = pipeStart;
         bottomPipe.y = Math.floor(Math.random() * (900 - 400 + 1) + 300);
         topPipe.y = bottomPipe.y - gap;
         score++;
         document.getElementById('score').innerHTML = `Score: ${score}`
     }
-    // We can rotate the bird by checking if vy is positive or negative
-    if (bird.y > canvas.height || bird.y < 0) {
+
+    // Collision Detection!
+    if (bird.y > canvas.height || bird.y < 0 || checkCollision(bird, bottomPipe) || checkCollision(bird, topPipe)) {
         location.reload();
     }
     window.requestAnimationFrame(draw);
 }
 
+function checkCollision(bird, pipe) {
+    let rect;
+    if (pipe.id === 'bot') {
+        rect = {
+            x: pipe.x,
+            y: pipe.y,
+            w: 100,
+            h: 1000
+        }
+    }
+    else {
+        rect = {
+            x: pipe.x,
+            y: 0,
+            w: 100,
+            h: pipe.y
+        }
+    }
+    let distX = Math.abs(bird.x - rect.x-rect.w/2);
+    let distY = Math.abs(bird.y - rect.y-rect.h/2);
 
+    if (distX > (rect.w/2 + bird.radius)) { return false; }
+    if (distY > (rect.h/2 + bird.radius)) { return false; }
+
+    if (distX <= (rect.w/2)) { return true; } 
+    if (distY <= (rect.h/2)) { return true; }
+
+    let dx=distX-rect.w/2;
+    let dy=distY-rect.h/2;
+    return (dx*dx+dy*dy<=(bird.radius*bird.radius));
+}
 
 init()
 
